a67c403b32dd13fedd8cdb254405db9d36ba0bac
Danil Harlan <danil.harlan@kallisto.dev>
2024-04-02 23:35:36 +0300

    city controller

--- lib/repositories/weather_details/weather_forecast_city_coordinate_repository.dart
+++ lib/repositories/weather_details/weather_forecast_city_coordinate_repository.dart
@@ -2,14 +2,12 @@ import 'package:dio/dio.dart';
 import 'models/city_coordinate.dart';
 
 const _host = 'http://api.openweathermap.org/geo/1.0/direct?';
-const enterCityName = 'Lviv';
-const _cityName = 'q=$enterCityName,804&limit=1';
 const _appID = '&appid=';
 const _apiKey = '4dbe24134496b55a1b13855ddf7c5847';
 
 class WeatherForecastCityCoordinateRepository {
-  Future<CityCoordinate> getCityCoordinate2(String enterCityName) async {
-    final url = '${_host}q=$enterCityName,804&limit=1$_appID$_apiKey';
+  Future<CityCoordinate> getCityCoordinate2(String cityName) async {
+    final url = '${_host}q=$cityName,804&limit=1$_appID$_apiKey';
 
     final response = await Dio().get(url);
 
